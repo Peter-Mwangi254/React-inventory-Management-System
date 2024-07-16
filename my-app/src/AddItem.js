86828a632668b653623ab977a498c4a19b97aa0b
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-17 01:49:47 +0300

    Inventory management system app

---
+++ my-app/src/AddItem.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+function AddItem(props) {
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState(0);
+    const [type, setType] = useState("");
+    const [brand, setBrand] = useState("");
+
+    const addItemButtonPressed = () => {
+        props.addItem({
+            name: name,
+            price: price,
+            type: type,
+            brand: brand,
+        });
+    };
+    return (
+        <div className="container">
+            <div className="row">
+            <h2>Add an item</h2>
+            </div>
+            <div className="row">
+            <label htmlFor="name-field">Name:</label>
+                <input
+                id="name-field"
+                type="text"
+                className="form-control"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+                <label htmlFor="price-field">Price:</label>
+                <input
+                id="price-field"
+                type="text"
+                className="form-control"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                />
+                <label htmlFor="type-field">Type:</label>
+                <input
+                id="type-field"
+                type="type"
+                className="form-control"
+                value={type}
+                onChange={(e) => setType(e.target.value)}
+                />
+                <label htmlFor="brand-field">Brand:</label>
+                <input
+                id="name-field"
+                type="text"
+                className="form-control"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+                />
+            </div>
+            <div className="row mt-3">
+            <button 
+            type="button" 
+            className="btn btn-primary"
+            onClick={addItemButtonPressed}>
+                Add Item
+                </button>
+            </div>
+                
+        </div>
+    );
+}
+
+export default AddItem;
---
+++ my-app/src/App.js
@@ -0,0 +1,101 @@
+import './App.css';
+import Searchbar from "./SearchBar";
+import AddItem from "./AddItem";
+import { useState, useEffect } from 'react';
+import ItemsDisplay from "./ItemsDisplay";
+
+function App() {
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+    .then((response) => response.json())
+    .then((data) => setData({ items: data}));
+    }, []);
+
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  };
+
+  const deleteItem = (item) => {
+    const items = data["items"];
+    const requestOptions = {
+      method: "DELETE"
+    }
+    fetch(`http://localhost:3000/items/${item.id}`, requestOptions).then(
+      (response) => {
+        if (response.ok) {
+          const idx = items.indexOf(item);
+          items.splice(idx, 1);
+          setData({ items: items });
+        }
+    })
+  }
+
+  const addItemToData = (item) => {
+    let items = data["items"];
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item)
+    };
+    fetch("http://localhost:3000/items", requestOptions)
+      .then((response) => 
+      response.json()
+    ).then((data) => { 
+      items.push(data);
+      setData({ items: items }); });
+
+   
+  };
+
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (!filters.name) {
+      return data;
+    }
+
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue;
+      }
+
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue;
+      }
+
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue;
+      }
+
+      filteredData.push(item);
+    }
+    return filteredData;
+  };
+
+  return (
+    <div className='container'>
+      <div className='row mt-3'>
+      <ItemsDisplay deleteItem={deleteItem}
+      items={filterData(data["items"])} />
+      </div>
+      <div className='row mt-3'>
+      <Searchbar updateSearchParams={updateFilters} /> 
+      </div>
+      <div className='row mt-3'>
+      <AddItem addItem={addItemToData} />
+      </div>
+    </div>
+  );
+  }
+
+export default App;
